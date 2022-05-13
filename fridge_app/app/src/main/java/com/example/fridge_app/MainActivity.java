db50d0e519f2d7558a17910b1b7cb3e625721c7e
pangsitmie <pangsitmie@github.com>
2022-05-13 16:52:33 +0800

    main buttons

--- fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
@@ -3,14 +3,35 @@ package com.example.fridge_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNewOrder,changeProgram, btnProgress, btnSettings, btnContactService, btnMaintenance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        btnNewOrder = findViewById(R.id.btnNewOrder);
+        changeProgram = findViewById(R.id.changeProgram);
+        btnProgress = findViewById(R.id.btnProgress);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnContactService = findViewById(R.id.btnContactService);
+        btnMaintenance = findViewById(R.id.btnMaintenance);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+
+
+
     }
 }
