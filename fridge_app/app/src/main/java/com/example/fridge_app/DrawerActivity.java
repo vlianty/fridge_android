aff47df69df3db6b69a749d1b8cc8f57489867af
vlianty <liantyviviciang@gmail.com>
2022-05-13 20:34:50 +0800

    SetIng

--- fridge_app/app/src/main/java/com/example/fridge_app/DrawerActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/DrawerActivity.java
@@ -2,19 +2,13 @@ package com.example.fridge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class EditExpiredDateActivity extends AppCompatActivity {
-
+public class DrawerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_expired_date);
-
-
+        setContentView(R.layout.activity_drawer);
     }
 }
--- fridge_app/app/src/main/java/com/example/fridge_app/EditButtonActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/EditButtonActivity.java
@@ -10,5 +10,6 @@ public class EditButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_button);
+
     }
 }
