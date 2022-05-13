95104f24c9505cc5629c1e20f814433f5e5b27da
vlianty <liantyviviciang@gmail.com>
2022-05-13 16:15:47 +0800

    Setting

---
+++ fridge_app/app/src/main/java/com/example/fridge_app/SettingActivity.java
@@ -0,0 +1,26 @@
+package com.example.fridge_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingActivity extends AppCompatActivity {
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+//        button2 = findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Intent intent = new Intent(getApplicationContext(), edit_button_activity.class);
+//                startActivity(intent);
+//            }
+//        }
+    }
+}
