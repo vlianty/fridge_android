dd11a34ecc03fc4efff8d263fc6903a92ae6ade4
chiehyuchiu <105184380+chiehyuchiu@users.noreply.github.com>
2022-05-16 00:28:53 +0800

    push

--- fridge_app/app/src/main/java/com/example/fridge_app/EditDrawerActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/EditDrawerActivity.java
@@ -2,6 +2,7 @@ package com.example.fridge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.view.Menu;
@@ -9,20 +10,54 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.util.Log;
+
 
 
 
 
 public class EditDrawerActivity extends AppCompatActivity {
 
+    Button submmit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_drawer);
+        submmit = findViewById(R.id.submmit);
         final EditText simpleEditText = (EditText) findViewById(R.id.nameinput);//get the id for edit text
 
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        mySpinner.setAdapter(new ArrayAdapter<GoodsType>(this, android.R.layout.simple_spinner_item, GoodsType.values()));
+
+        submmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(getApplicationContext(), NewDrawerActivity.class);
+                //startActivity(intent);
+                //Log.d("test","1234");
+                EditText nameinput = (EditText) findViewById(R.id.nameinput);
+                String name = nameinput.getText().toString();
+                EditText minWeightInput = (EditText)findViewById(R.id.editTextNumber);
+                float minWeight = Float.parseFloat(minWeightInput.getText().toString());
+                Spinner spinner = (Spinner)findViewById(R.id.spinner);
+                int gt = spinner.getSelectedItemPosition();
+                GoodsType goodsType = GoodsType.Cooked;
+                
+
+                Log.d("test", name);
+                Log.d("test", Float.toString(minWeight));
+                Log.d("test", Integer.toString(gt));
+                //TextView tv = (TextView) findViewById(R.id.nameinput);
+                //tv.setText(input);
+            }
+        });
     }
 }
