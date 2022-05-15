55035855c68a1e75c32a67a539c5904ef8812fe7
vlianty <liantyviviciang@gmail.com>
2022-05-15 20:00:14 +0800

    OrderAnongShop

--- fridge_app/app/src/main/java/com/example/fridge_app/OrderActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/OrderActivity.java
@@ -2,13 +2,25 @@ package com.example.fridge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OrderActivity extends AppCompatActivity {
+    Button VeFamBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        VeFamBut = findViewById(R.id.VeFamBut);
+        VeFamBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),VegetableSupplierActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- fridge_app/app/src/main/java/com/example/fridge_app/VegetableSupplierActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/VegetableSupplierActivity.java
@@ -1,13 +1,27 @@
 package com.example.fridge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class VegetableSupplierActivity extends AppCompatActivity {
+    ImageButton anongButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vegetable_supplier);
+        anongButton = findViewById(R.id.anongButton);
+        anongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),AnongShopActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
