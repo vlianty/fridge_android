e54da9d543988467c9e790a42a7094236844af9e
vlianty <liantyviviciang@gmail.com>
2022-05-15 17:06:16 +0800

    SupplierList

---
+++ fridge_app/app/src/main/java/com/example/fridge_app/VegetableSupplierActivity.java
@@ -0,0 +1,13 @@
+package com.example.fridge_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VegetableSupplierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vegetable_supplier);
+    }
+}
