b897a811f278705f948d2e1a4a21d10a146b0f49
DaDioDa <56485296+DaDioDa@users.noreply.github.com>
2022-05-21 22:48:23 +0800

    opt

--- fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
@@ -71,10 +71,7 @@ public class MainActivity extends AppCompatActivity {
         btndrawer = findViewById(R.id.btndrawer);
 
         //Controller
-        FridgeDrawer[][] fridgeDrawer = new FridgeDrawer[3][2];
-        ctrl = new controller(fridgeDrawer);
-        //ctrl.setDrawer(0,0,new FridgeDrawer(DrawerMode.Defrost,new Goods(1,GoodsType.Cooked,null,"Apple Pie"),100,LocalDate.now()));
-        //Log.d("test",ctrl.fd[0][0].goods.g_Name);
+        ctrl = controller.getInstance();
 
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent intent = new Intent(getApplicationContext(), NewDrawerActivity.class);
-                intent.putExtra("ctrl",ctrl);
                 startActivity(intent);
             }
         });
@@ -109,8 +105,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
         }
     }
 
