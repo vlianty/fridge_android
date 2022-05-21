9b484bbf1c8167a40af88c8e7ce2e43f7c67bf4e
yuyudess <A11023019@gemail.yuntech.edu.tw>
2022-05-21 17:53:32 +0800

    notify_ver.2

--- fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/MainActivity.java
@@ -27,9 +27,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /**檢查手機版本是否支援通知；若支援則新增"頻道"*/
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        //檢查手機版本是否支援通知，若支援則新增channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
             NotificationChannel channel = new NotificationChannel(
                     CHANNEL_ID, "DemoCode", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
@@ -37,26 +37,23 @@ public class MainActivity extends AppCompatActivity {
             manager.createNotificationChannel(channel);
         }
 
-
         Button btDefault;
         btDefault = findViewById(R.id.notification);
         btDefault.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                NotificationCompat.Builder builder
-                        = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID)
+                NotificationSender ns = new NotificationSender();
+                ns.builder = new NotificationCompat.Builder(MainActivity.this,CHANNEL_ID)
                         .setSmallIcon(R.drawable.carrot)
-                        .setContentTitle("低庫存警告!")
-                        .setContentText("該補貨囉")
+                        .setContentTitle(ns.WarnComponentTitle()) //通知的title，NotificationSender有可以切換的文字
+                        .setContentText(ns.WarnComponentText())//通知的內文，因為還不知道要在哪判斷，所以先這樣寫
                         .setAutoCancel(true)
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .setCategory(NotificationCompat.CATEGORY_MESSAGE);
-
-
                 NotificationManagerCompat notificationManagerCompat
                         = NotificationManagerCompat.from(MainActivity.this);
-                notificationManagerCompat.notify(1,builder.build());
+                notificationManagerCompat.notify(1,NotificationSender.builder.build());
+
             }
         });
 
@@ -111,6 +108,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        }
     }
-
+}
--- fridge_app/app/src/main/java/com/example/fridge_app/NotificationSender.java
+++ fridge_app/app/src/main/java/com/example/fridge_app/NotificationSender.java
@@ -1,26 +1,44 @@
 package com.example.fridge_app;
 
-import static android.content.Context.NOTIFICATION_SERVICE;
-
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationSender{
+    public static NotificationCompat.Builder builder;
+    public String GoodName = "大高麗菜";
 
-    void alertLowStock()
+    public  String AltLowStockTitle()
     {
-
+        return "低庫存警告!";
     }
+    public  String AltLowStockText()
+    {
+        return "該補貨囉";
+    }
+    public  String ExpTitle()
+    {
+        return "到期通知!";
+    }
+    public  String  ExpText()
+    {
+        return GoodName + "即將到期!!!";
+    }
+    public  String WrongTempTitle()
+    {
+        return "溫度異常!";
+    }
+    public  String  WrongTempText()
+    {
+        return "快去確認一下冰箱的狀況!!";
+    }
+    public  String WarnComponentTitle()
+    {
+        return "零件異常!";
+    }
+    public  String  WarnComponentText()
+    {
+        return "有零件壞掉了，快聯繫客服!";
+    }
+
+
 
 }
